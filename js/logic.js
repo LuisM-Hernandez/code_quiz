4160a7b4c43fa168ada76aaaa2882ca526cc85e7
Luis <m.luishernandez@outlook.com>
2020-12-21 18:25:50 -0500

    set timer

--- js/logic.js
+++ js/logic.js
@@ -3,6 +3,7 @@ var questionArray = document.getElementById("questions");
 var timerEl = document.getElementById("score-timer");
 
 
+
 var questionTitle = document.getElementById("question-title");
 var questionChoices = document.getElementById("choices");
 var startBtn = document.getElementById("button-start");
@@ -19,11 +20,12 @@ startScreen = document.getElementById("start-screen");
 startScreen.setAttribute("class", "hide");
 questionArray.setAttribute("class", "start");
 getQuestions();
+setTimer();
 
 }
 
 function getQuestions(){
-   
+
 questionTitle.textContent = questions[currentQuestionIndex].title;
 
 questionChoices.textContent = questions[currentQuestionIndex].choices;
@@ -33,25 +35,15 @@ questionChoices.textContent = questions[currentQuestionIndex].choices;
 function setTimer() {
     var timerInterval = setInterval(function() {
       secondsLeft--;
-      timeEl.textContent = secondsLeft + " seconds left till colorsplosion.";
-  
+      timerEl.textContent = secondsLeft + " timer display";
       if(secondsLeft === 0) {
         clearInterval(timerInterval);
-        sendMessage();
       }
-  
     }, 1000);
   }
+
+
   
-  function sendMessage() {
-    timeEl.textContent = " ";
-  
-    var imgEl = document.createElement("img");
-  
-    imgEl.setAttribute("src", "images/image_1.jpg");
-    mainEl.appendChild(imgEl);
-  
-  }
 
 
 
