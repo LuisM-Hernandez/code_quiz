bccc4210549d3216ce543436f16b1abd4a85feeb
Luis <m.luishernandez@outlook.com>
2020-12-21 17:52:03 -0500

    Finish hiding and making appear the questions. Working on timer

--- js/logic.js
+++ js/logic.js
@@ -1,12 +1,58 @@
-// Select start button
-var startBtn = document.getElementById("start");
+var startScreen = document.getElementById("start-screen");
+var questionArray = document.getElementById("questions");
+var timerEl = document.getElementById("score-timer");
 
-// Call startQuiz function on button click
+
+var questionTitle = document.getElementById("question-title");
+var questionChoices = document.getElementById("choices");
+var startBtn = document.getElementById("button-start");
+
+var currentQuestionIndex = 0;
+var secondsLeft = 200;
+
+
 startBtn.onclick = startQuiz;
 
-// Function to start quiz
+
 function startQuiz() {
-var startScreen = document.querySelector("#start-screen");
+startScreen = document.getElementById("start-screen");
 startScreen.setAttribute("class", "hide");
+questionArray.setAttribute("class", "start");
+getQuestions();
 
 }
+
+function getQuestions(){
+   
+questionTitle.textContent = questions[currentQuestionIndex].title;
+
+questionChoices.textContent = questions[currentQuestionIndex].choices;
+
+}
+
+function setTimer() {
+    var timerInterval = setInterval(function() {
+      secondsLeft--;
+      timeEl.textContent = secondsLeft + " seconds left till colorsplosion.";
+  
+      if(secondsLeft === 0) {
+        clearInterval(timerInterval);
+        sendMessage();
+      }
+  
+    }, 1000);
+  }
+  
+  function sendMessage() {
+    timeEl.textContent = " ";
+  
+    var imgEl = document.createElement("img");
+  
+    imgEl.setAttribute("src", "images/image_1.jpg");
+    mainEl.appendChild(imgEl);
+  
+  }
+
+
+
+
