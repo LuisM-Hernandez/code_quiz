dc2e023ef19b282487296deeb90a2d94e77deae4
Luis <m.luishernandez@outlook.com>
2020-12-21 22:59:18 -0500

    added buttons to choices

--- js/logic.js
+++ js/logic.js
@@ -1,14 +1,16 @@
 var startScreen = document.getElementById("start-screen");
 var questionArray = document.getElementById("questions");
 var timerEl = document.getElementById("score-timer");
-
-
-
-var questionTitle = document.getElementById("question-title");
-var questionChoices = document.getElementById("choices");
 var startBtn = document.getElementById("button-start");
+var questionTitle = document.getElementById("question-title");
 
-var currentQuestionIndex = 0;
+var choiceBtn;
+
+
+var choice = document.querySelector("#choices");
+
+
+var currentQuestionIndex = -1;
 var secondsLeft = 200;
 
 
@@ -25,17 +27,25 @@ setTimer();
 }
 
 function getQuestions(){
-
+// Need choices to appear in buttons
+currentQuestionIndex++;
 questionTitle.textContent = questions[currentQuestionIndex].title;
 
-questionChoices.textContent = questions[currentQuestionIndex].choices;
+choiceBtn = questions[currentQuestionIndex].choices;
+var correctBtn = questions[currentQuestionIndex].answer;
 
+for (var i = 0; i < choiceBtn.length; i++){
+    var btnCreator = document.createElement("button");
+    btnCreator.textContent = choiceBtn[i];
+    displayBtn = choice.appendChild(btnCreator).setAttribute("class", "btns");
+    
+}
 }
 
 function setTimer() {
     var timerInterval = setInterval(function() {
       secondsLeft--;
-      timerEl.textContent = secondsLeft + " timer display";
+      timerEl.textContent = " timer display" + secondsLeft  ;
       if(secondsLeft === 0) {
         clearInterval(timerInterval);
       }
